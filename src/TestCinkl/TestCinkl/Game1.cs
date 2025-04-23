57d6948574bc32aa657387eda41d0fe19e814e62
Stanislav Cinkl <stanislav.cinkl@student.spsmb.cz>
2025-04-23 10:23:00 +0200

    part 2

--- src/TestCinkl/TestCinkl/Game1.cs
+++ src/TestCinkl/TestCinkl/Game1.cs
@@ -10,6 +10,8 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private Texture2D _spriteSheet;
     private Vector2 _playerPosition;
+    private Vector2 _velocity;
+    private const float Gravity = 0.3f;
 
     public Game1()
     {
@@ -21,6 +23,7 @@ public class Game1 : Game
     protected override void Initialize()
     {
         _playerPosition = new Vector2(100, 100);
+        _velocity = Vector2.Zero;
 
         base.Initialize();
     }
@@ -38,7 +41,16 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+        
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+        // Aplikuj gravitaci na vertikální složku rychlosti
+        _velocity.Y += Gravity;
+
+        // Aktualizuj pozici podle rychlosti
+        _playerPosition += _velocity;
+        
+        
         // TODO: Add your update logic here
 
         base.Update(gameTime);
